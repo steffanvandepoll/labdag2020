6c0978b79bb962c5d421d78301f931ec8b67474b
Salem <mo.salem@capgemini.com>
2020-03-06 15:59:20 +0100

    change Inspect return type

--- src/actions/Inspect.js
+++ src/actions/Inspect.js
@@ -1,7 +1,7 @@
 function Inspect(items, key) {
     key = key.toLowerCase();
     let selected = items[key];
-    return selected;
+    return {responseText: selected};
 };
 
 export default Inspect;
