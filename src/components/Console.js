5633473cab6d0bc8a88310310f961ff35b3665b6
Steffan van de Poll <steffanvandepoll@gmail.com>
2020-03-06 11:00:17 +0100

    Add console component

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+  background: black;
+  border-radius: 8px;
+  border: solid 2px white;
+  width: 450px;
+  height: calc(100vh - 70px);
+  color: #265d00;
+  font-family: 'Source Code Pro', monospace;
+  text-align: left;
+  padding:10px;
+  p{
+    padding: 0;
+    margin:0;
+  }
+`
 
 class Console extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello Console </h1>
-      </div>
+      <Container>
+        <p>Hello %3e Console </p>
+      </Container>
     );
   }
 }
