89cbb5462f34ef7a43ebae166fe8aba34199b162
Messaoudi <ilyass.messaoudi@capgemini.com>
2020-03-06 12:59:40 +0100

    unlock function

---
+++ src/actions/unlock.js
@@ -0,0 +1,24 @@
+
+function unlock(laptopName, password){
+
+          switch(laptopName) {
+            case 'Iris':
+                if(password === ?){
+                  return 'Laptop Iris has been unlocked';
+                }
+            break;
+            case 'Jasmine':
+                if(password === ?){
+                  return 'Laptop Jasmine has been unlocked';
+                }
+            break;
+            case 'Kim':
+                if(password === ?){
+                  return 'Laptop Kim has been unlocked';
+                }
+            break;
+            default:
+            return 'Laptop not found!'
+          }
+
+}
