08cf7968917e3d63a667d87732b27ff87ab0e374
Messaoudi <ilyass.messaoudi@capgemini.com>
2020-03-06 13:36:41 +0100

    Unlock Func

--- src/actions/unlock.js
+++ src/actions/unlock.js
@@ -1,31 +1,20 @@
 function Unlock(laptopName, password){
 
-          switch(laptopName) {
-            case 'Iris':
-                    if(password === 'irisPass'){
-                       return 'You have unlocked laptop Iris';
-                    } else{
-                       return 'Wrong password for laptop Iris';
-                    }
-            break;
-            case 'Jasmine':
-                    if(password === 'jasminPass'){
-                       return 'You have unlocked laptop Jasmine';
-                    } else{
-                       return 'Wrong password for laptop Jasmine';
-                    }
-            break;
-            case 'Kim':
-                  if( password === 'kimPass'){
-                     return 'You have unlocked laptop Kim';
-                  } else{
-                     return 'Wrong password for laptop Kim';
-                  }
-            break;
-            default:
-                  return 'Laptop not found!';
+          if(laptopName === 'Iris' || laptopName === 'Jasmine' || laptopName === 'Kim'){
+              return PasswordCheck(laptopName, password);
+          }else{
+              return 'Laptop not found!';
           }
 
+}
+
+function PasswordCheck(laptopName, password){
+
+      if(password === 'irisPass' || password === 'jasminPass' || password === 'kimPass' ){
+            return 'You have unlocked laptop ' + laptopName;
+      }else{
+            return 'Wrong password for laptop ' + laptopName;
+      }
 }
 
 
