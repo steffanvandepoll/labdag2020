2ca4dd0b4b15b7b9d555332bf6e50f07bdba6b0e
Steffan van de Poll <steffanvandepoll@gmail.com>
2020-03-06 10:39:20 +0100

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Console from './components/Console'
 
 function App() {
   return (
     <div className="App">
+      <Console />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Console.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Console extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Console </h1>
+      </div>
+    );
+  }
+}
+
+export default Console;
